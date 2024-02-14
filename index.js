3e3b7c427734b2db451dbbaee7087779a42a56b5
Brandon <blor15@hotmail.com>
2024-02-13 17:42:57 -0700

    User is able to deposit money

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// Collecting the deposit money from the user
-const deposit = () => {};
+const prompt = require("prompt-sync")();
 
-deposit();
+// Collecting the deposit money from the user
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+  while (true) {
+    // parseFloat takes a string and converts it into it's floating value
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+
+const depositAmount = deposit();
